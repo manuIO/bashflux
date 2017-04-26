09e10641a1f3ffdcec2a5864f1c2c17160f05a25
Manuel Imperiale <manuel.imperiale@gmail.com>
2017-04-26 12:04:50 +0200

    rm unused func
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- cmd/utils.go
+++ cmd/utils.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"bytes"
-	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -10,13 +8,6 @@ import (
 
 	"github.com/hokaccha/go-prettyjson"
 )
-
-//dont do this, see above edit
-func prettyJSON(b []byte) ([]byte, error) {
-	var out bytes.Buffer
-	err := json.Indent(&out, b, "", "    ")
-	return out.Bytes(), err
-}
 
 func PrettyJson(body string) string {
 	pj, err := prettyjson.Format([]byte(body))
